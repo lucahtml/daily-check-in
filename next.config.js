7e6ea110386e95ebf68d39966376bdf66589d1c5
Luca Thomas <lucathomas@example.com>
2025-02-26 10:59:24 +0100

    Prepare for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/daily-check-in' : '',
-  images: {
-    unoptimized: true,
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
